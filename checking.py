cdb40b37fff36a4ad1dcb11159c32e816fbe0dbb
lynda-trad <lynda.trad@laplateforme.io>
2021-10-20 14:27:17 +0200

    checkline and checkcolumn draft in checking.py

--- checking.py
+++ checking.py
@@ -1,3 +1,42 @@
 def checkLine(numberGrid, coordinates):
-    for i in range(1, 9):
-        print(i)
+    i = coordinates[0]
+
+    boolean = False     # not on line
+    hypothesis = []
+
+    for number in range(1, 9):
+        print(number)
+        while not boolean:                          # while number is not found on line, we keep looking
+            for jx in len(range(numberGrid[i])):
+                if number == numberGrid[i][jx]:     # found number on line
+                    boolean = True
+            if not boolean:
+                hypothesis.append(number)
+    return hypothesis
+
+
+def checkColumn(numberGrid, coordinates):
+    j = coordinates[1]
+
+    boolean = False     # not on column
+    hypothesis = []
+
+    for number in range(1, 9):
+        print(number)
+        while not boolean:                          # while number is not found on column, we keep looking
+            for ix in len(range(numberGrid[j])):
+                if number == numberGrid[ix][j]:     # found number on column
+                    boolean = True
+            if not boolean:
+                hypothesis.append(number)
+    return hypothesis
+
+
+def checkBox(numberGrid, coordinates):
+    i = coordinates[0]
+    j = coordinates[1]
+
+    boolean = False  # not on column
+    hypothesis = []
+
+    return hypothesis
